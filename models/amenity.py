f4144a657bd086588c6bb56fe5e761827fa51bc2
firas <ghrairi237@gmail.com>
2023-11-30 20:30:19 +0100

    amenity

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Project 0x00. AirBnB clone - The console
+    Task 10
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Amenity class
+    """
+    name = ""
