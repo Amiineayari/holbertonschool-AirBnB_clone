6e5ab42c4f88c35ffe143ac7240ac73c6eff1dac
firas <ghrairi237@gmail.com>
2023-11-30 20:30:29 +0100

    city

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Project 0x00. AirBnB clone - The console
+    Task 10
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ City class
+    """
+    state_id = ""
+    name = ""
